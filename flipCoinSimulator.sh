a82698ef6e06640cc8319fe835715efc8cdff4cf
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-14 23:58:31 +0530

    [Manisha]: Perform Multiple Flips And Declare results

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,26 @@
 echo "Welcome to Flip Coin Game"
 
+read -p "Enter the number of times you want to flip coin: " num
+heads=0
+tails=0
 function flipCoin() {
-if(($((RANDOM % 2)) == 1 ))
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+
+for((i=0;i<num;i++))
+do
+	if(($((RANDOM % 2)) == 1 ))
+	then
+		heads=$((heads+1))
+	else
+		tails=$((tails+1))
+	fi
+done
+
+	if(($heads>$tails))
+	then
+		echo "Heads won by total count: " $heads
+	else
+		echo "tails won by total count: " $tails
+	fi
 }
 
 #!Calling Function
